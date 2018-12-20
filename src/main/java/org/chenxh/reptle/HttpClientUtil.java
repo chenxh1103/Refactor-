4db5e6555eaca81f261af32f52ac1f40d599cc82
陈相宏 <l>
2018-12-20 16:26:16 +0800

    环境搭建

--- src/main/java/org/chenxh/reptle/HttpClientUtil.java
+++ src/main/java/org/chenxh/reptle/HttpClientUtil.java
@@ -1,23 +1,17 @@
 package org.chenxh.reptle;
 
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
