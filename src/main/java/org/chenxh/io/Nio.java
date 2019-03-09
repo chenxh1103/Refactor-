e03b4525e8aa09c55f08fdcd9734a997d16dafd1
陈相宏 <l>
2019-03-09 10:06:23 +0800

    io学习

--- src/main/java/org/chenxh/io/Nio.java
+++ src/main/java/org/chenxh/io/Nio.java
@@ -1,4 +1,25 @@
 package org.chenxh.io;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
 //http://www.importnew.com/22623.html
+//nio 更多用来用于客户端与服务端的使用
 public class Nio {
+
+    private void fileNio() throws Exception{
+        File inputFile = new File("");
+        FileInputStream fileInputStream = new FileInputStream(inputFile);
+        File outFile = new File("");
+        FileOutputStream fileOutputStream = new FileOutputStream(outFile);
+        FileChannel fileChannel = fileInputStream.getChannel();
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+        while (fileChannel.read(byteBuffer)!=-1){
+
+        }
+    }
+
 }
