324915966a5acd756150806d2de74b0314598f4d
chenxh <605025758@qq.com>
2019-03-25 23:18:36 +0800

    新建工厂方法注入springBean

--- src/main/java/org/chenxh/reptle/pipeline/BiQuGeFilePipeline.java
+++ src/main/java/org/chenxh/reptle/pipeline/BiQuGeFilePipeline.java
@@ -1,18 +1,20 @@
 package org.chenxh.reptle.pipeline;
 
+import org.chenxh.web.factory.SpringServiceFactory;
 import org.chenxh.web.eneity.Book;
 import org.chenxh.web.service.BookService;
-import org.chenxh.web.serviceImpl.BookServiceImpl;
 import us.codecraft.webmagic.ResultItems;
 import us.codecraft.webmagic.Task;
 import us.codecraft.webmagic.pipeline.Pipeline;
 import us.codecraft.webmagic.utils.FilePersistentBase;
 
 public class BiQuGeFilePipeline extends FilePersistentBase implements Pipeline {
+
     @Override
     public void process(ResultItems resultItems, Task task) {
         Book book = resultItems.get("book");
-        BookService bookService = new BookServiceImpl();
-        bookService.insertBook(book);
+        //BookService bookService = new BookServiceImpl();
+        BookService bookService = SpringServiceFactory.getBean(BookService.class);
+        bookService.selectBook(book);
     }
 }
--- src/test/org/chenxh/reptle/pipeline/BiQuGeFilePipelineTest.java
+++ src/test/org/chenxh/reptle/pipeline/BiQuGeFilePipelineTest.java
@@ -2,6 +2,9 @@ package org.chenxh.reptle.pipeline;
 
 import org.chenxh.AbstractTest;
 import org.chenxh.reptle.documentParser.biQuGe.BiQuGePageProcessor;
+import org.chenxh.web.factory.SpringServiceFactory;
+import org.chenxh.web.eneity.Book;
+import org.chenxh.web.service.BookService;
 import org.junit.Test;
 import us.codecraft.webmagic.Spider;
 
@@ -13,5 +16,13 @@ public class BiQuGeFilePipelineTest extends AbstractTest {
                 .addUrl("https://www.sbiquge.com/31_31715")//遮天抓包
                 .addPipeline(new BiQuGeFilePipeline())
                 .run();
+    }
+    @Test
+    public void t(){
+        Book book = new Book();
+        book.setBookId(1);
+        book.setBookName("chenxh");
+        BookService bookService = SpringServiceFactory.getBean(BookService.class);
+        bookService.selectBook(book);
     }
 }
