4cec8d16fd5c87184487376667c0a69a4e7eeed3
陈相宏 <l>
2019-03-27 18:05:44 +0800

    restful修复

--- src/main/java/org/chenxh/web/controller/BookController.java
+++ src/main/java/org/chenxh/web/controller/BookController.java
@@ -1,9 +1,13 @@
 package org.chenxh.web.controller;
 
+import org.chenxh.web.entity.Book;
+import org.chenxh.web.enums.BookState;
 import org.chenxh.web.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BookController {
@@ -13,7 +17,21 @@ public class BookController {
     @RequestMapping(value = "/index")
     public String index(){
 
-
         return "index";
+    }
+    @RequestMapping(value = "/{bookId}/{sortId}.html")
+    public String book(@PathVariable String bookId, @PathVariable String sortId){
+        System.out.println(bookId);
+        return "index";
+    }
+    @ResponseBody
+    @RequestMapping(value = "/{bookId}")
+    public Book selectBook(@PathVariable("bookId") String bookId){
+        Book book = new Book();
+        book.setBookId(1);
+        book.setBookName("chen");
+        book.setAuthor("三大");
+        book.setBookState(BookState.Serial);
+        return book;
     }
 }
